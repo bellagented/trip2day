dced1e90e80f7857d4a1517ec1030cc584b912b7
oraldo-doci <oraldo.doci@gmail.com>
2020-11-30 14:53:44 +0100

    day_planner

--- src/Pages/Components/IconPreview.js
+++ src/Pages/Components/IconPreview.js
@@ -5,7 +5,16 @@ export default function IconPreview(props) {
     return (
       <div>
         {" "}
-        <img style={{width: '150px',height:'150px',border:'2px solid black', borderRadius: '50%'}} src={element.img} alt="preview" />
+        <img
+          style={{
+            width: "150px",
+            height: "150px",
+            border: "2px solid black",
+            borderRadius: "50%",
+          }}
+          src={element.img}
+          alt="preview"
+        />
         <h3>{element.name}</h3>
       </div>
     );
--- src/Pages/ListPlanner.js
+++ src/Pages/ListPlanner.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function ListPlanner(){
-return(<h1>EHI RICK, SONO UN ListPlanner</h1>)
+import Demo from "./Components/Schedule";
+
+export default function ListPlanner() {
+  return (
+    <>
+      <h1>EHI RICK, SONO UN ListPlanner</h1>
+      <Demo />
+    </>
+  );
 }

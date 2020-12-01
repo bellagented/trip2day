05695e7f5b8bbda1e89d8e3f50a54df09297c90e
bellagented <bellagented@gmail.com>
2020-12-01 15:59:16 +0100

    tolta navbar da pagina login

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import "../styles/Navbar.css";
 
-export default function Navbar() {
+export default function Navbar(props) {
   const history = useHistory();
-  return (
+  if(history.location.pathname !== '/'){return(
     <div style={{display: "flex" }} className='navbar'>
-      <img />
       <h2 className='appname'>Trip2Day</h2>
       <Link className='navlink' to="/home">Home</Link>
       <Link className='navlink' to="/planner">Planners</Link>
       <Link className='navlink' to="/archive">Archives</Link>
-    </div>
-  );
+      <p>{history.location.pathname}</p>
+    </div>)}
+else{return <div/>}
 }

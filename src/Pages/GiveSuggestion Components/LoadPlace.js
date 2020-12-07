eb2cecdacc3083ead949c539ba196a757a5b6503
bellagented <bellagented@gmail.com>
2020-12-07 11:31:44 +0100

    risolto bug form

--- src/Pages/GiveSuggestion Components/LoadPlace.js
+++ src/Pages/GiveSuggestion Components/LoadPlace.js
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
-
+import React from "react";
 
 export default function LoadPlace(props) {
-  
-
-  
   const handleChange = (e) => {
-    props.setEvent(
-      props.suggestion.find((suggestion) => {
-        return suggestion.where === e.target.value;
-      })
-    );
+    if (e.target.value === "-select a plan") {props.setEvent({activity:[{activity:'select a place'}]})
+    } else {
+      props.setEvent(
+        props.suggestion.find((suggestion) => {
+          return suggestion.where === e.target.value;
+        })
+      );
+    }
   };
-
 
   return (
     <>
@@ -27,9 +25,7 @@ export default function LoadPlace(props) {
           <option>-select a plan</option>
           {props.suggestion.map((mysuggestion) => {
             return (
-              <option key={mysuggestion.where}>
-                {mysuggestion.where}
-              </option>
+              <option key={mysuggestion.where}>{mysuggestion.where}</option>
             );
           })}
         </select>

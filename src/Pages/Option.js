b75e341dee004c573f464dbbd7217c4bc2bd1c43
Lorenzo Martinoli <lo.martinoli@gmail.com>
2020-12-10 14:25:38 +0100

    UPLOAD IMMAGINI, UPLOAD IMMAGINI

--- src/Pages/Option.js
+++ src/Pages/Option.js
@@ -1,10 +1,46 @@
-import React from "react";
-import Upload from "./Components/uploadButton.js";
-export default function Option() {
+import React, { useRef, useState } from "react";
+import axios from "axios";
+
+
+export default function UploadImage(props) {
+
+  const [file, setFile] = useState("");
+  const [data, getFile] = useState({name: "", path: ""});
+  const el = useRef();
+
+  const handleChange = (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+    setFile(file);
+  }
+
+  const uploadFile = () => {
+    const formData = new FormData();
+    formData.append("file", file);
+    axios.post("http://localhost:3001/upload", formData)
+    .then(res => {
+      console.log(res);
+      getFile({
+        name: res.data.name,
+        path: "http://localhost:3001" + res.data.path
+      })
+    }).then(() => {props.setPhoto(data.path)})
+    .catch(err => console.log(err))
+  }
+
   return (
-    <>
-      <h1>EHI RICK, SONO UN Option</h1>
-      <Upload />
-    </>
+    <div>
+      <div className="file-upload">
+        <input type="file" ref={el} onChange={handleChange} />
+        <button onClick={uploadFile} className="upbutton">
+          Upload
+        </button>
+            <hr />
+        {/* displaying received image*/}
+        {/* <div><textarea value={data.path} onChange={uploadFile} /></div> */}
+        {data.path && <img src={data.path} alt={data.name} style={{width: 700}}/>}
+        {console.log(data)}
+      </div>
+  </div>
   );
 }

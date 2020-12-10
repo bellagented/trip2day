83a41536d6828720e37745d0e843882d11872f46
bellagented <bellagented@gmail.com>
2020-12-10 09:16:57 +0100

    tolto icon preview extra

--- src/Pages/Home component/PreviewPlanner.js
+++ src/Pages/Home component/PreviewPlanner.js
@@ -52,8 +52,6 @@ const id = Math.random().toString(16).substr(8, 10);
 
       <div className="contaniterplans">
         <IconPreviewPlanner array={props.planners} path={'/planner/'}/>
-        <IconPreviewPlanner array={props.planners} path={'/planner/'}/>
-        <IconPreviewPlanner array={props.planners} path={'/planner/'}/>
   
         <div className="preview"  onClick={()=>{newPlan();
           history.push('http://localhost:3001/planner/' + id)}}> 
